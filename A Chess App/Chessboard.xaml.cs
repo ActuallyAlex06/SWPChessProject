45d3f4bb65f6b92a44a1d9f91b9c4a1abbe1d568
staffnerluca <staffnerluca@gmail.com>
2023-05-07 19:07:41 +0200

    only some tests. The answer was not found

--- A Chess App/Chessboard.xaml.cs
+++ A Chess App/Chessboard.xaml.cs
@@ -1,3 +1,5 @@
+//using Android.Bluetooth;
+
 namespace A_Chess_App;
 
 public partial class Chessboard : ContentPage
@@ -9,7 +11,35 @@ public partial class Chessboard : ContentPage
 
 	public void FieldClicked(object sender, EventArgs e)
 	{
-		Button btnonboard = sender as Button;
-		
-	}
+        ImageButton ibtn = (ImageButton)sender;
+        ImageButton field = FindByName(ibtn.StyleId) as ImageButton;
+        field.Source = "C:\\Users\\User\\source\\repos\\SWChessProject\\A Chess App\\Resources\\Images\\Images\\kingb";
+        foreach (var x in board)
+        {
+            if (x is ImageButton)
+            {
+                ImageButton y = x as ImageButton;
+                if(y.StyleId == ibtn.StyleId)
+                {
+                    ImageButton z = (ImageButton)FindByName(y.StyleId);
+                    z.Source = "C:\\Users\\User\\source\\repos\\SWChessProject\\A Chess App\\Resources\\Images\\Images\\kingb";
+                    //throw new NotFiniteNumberException();
+                }
+            }
+        }
+
+    }
+
+    public List<ImageButton> GetAllFields()
+    {
+        List<ImageButton> li = new List<ImageButton>();
+        foreach (var x in board)
+        {
+            if (x is ImageButton)
+            {
+                li.Add((ImageButton)x);
+            }
+        }
+        return li;
+    }
 }
