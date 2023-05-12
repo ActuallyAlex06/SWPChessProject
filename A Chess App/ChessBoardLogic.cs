42a9ed0d3e81e7637b0a2d110633990d92902280
ActuallyBecause <aschreyer574@gmail.com>
2023-05-12 02:27:07 +0200

    Allnighter

---
+++ A Chess App/ChessBoardLogic.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Chess_App
+{
+    public static class ChessBoardLogic
+    { 
+    //{
+    //    static List<string> coord1 = new List<string> { "A", "B", "D", "E", "F", "G", "H" };
+
+    //    public static int BlackPawnMath(int pos)
+    //    {
+    //        return pos - 1;
+    //    }
+
+    //    public static int WhitePawnMath(int pos)
+    //    {
+    //        return pos + 1;
+    //    }
+
+    //    public List<string> FigureOutPossibleMoves(string piece, string coordinates)
+    //    {
+    //        List<string> moves = new List<string> { };
+    //        specifics = coordinates.Split(" ");
+
+    //        if (piece.Equals("pawnb.png"))
+    //        {
+    //            if (specifics[1].Equals("7"))
+    //            {
+    //                moves.Add(specifics[0] + "5");
+    //            }
+
+    //            if (CheckButtonPositions(specifics, piece))
+    //            {
+    //                moves.Add(specifics[0] + ChessBoardLogic.BlackPawnMath(int.Parse(specifics[1])));
+    //            }
+
+    //        }
+    //        else if (piece.Equals("pawnw.png"))
+    //        {
+
+    //            if (specifics[1].Equals("2") && A3.ImageSource == null || specifics[1].Equals("2") && A4.ImageSource == null)
+    //            {
+    //                moves.Add(specifics[0] + "4");
+    //            }
+
+    //            moves.Add(specifics[0] + ChessBoardLogic.WhitePawnMath(int.Parse(specifics[1])));
+
+    //        }
+
+    //        return moves;
+    //    }
+
+
+    //    private bool CheckButtonPositions(string[] coordinates, string piece)
+    //    {
+    //        List<Button> lstbuttons = GetEachButton();
+    //        switch (piece)
+    //        {
+    //            case "pawnb.png":
+    //                foreach (Button button in lstbuttons)
+    //                {
+    //                    if (button.Text.Equals(specifics[0] + " " + ChessBoardLogic.BlackPawnMath(int.Parse(specifics[1]))))
+    //                    {
+    //                        if (button.ImageSource != null)
+    //                        {
+    //                            return false;
+    //                        }
+    //                    }
+    //                    else { return true; }
+    //                }
+    //                break;
+    //        }
+    //        return true;
+    //    }
+
+
+    //    private List<Button> GetEachButton()
+    //    {
+    //        {
+    //            List<Button> li = new List<Button>();
+    //            foreach (var x in board)
+    //            {
+    //                if (x is Button)
+    //                {
+    //                    li.Add((Button)x);
+    //                }
+    //            }
+    //            return li;
+    //        }
+    //    }
+    }
+}
--- A Chess App/Chessboard.xaml.cs
+++ A Chess App/Chessboard.xaml.cs
@@ -1,3 +1,6 @@
+
+using Microsoft.Maui.Controls.Internals;
+
 namespace A_Chess_App;
 
 public partial class Chessboard : ContentPage
@@ -6,42 +9,206 @@ public partial class Chessboard : ContentPage
 	{
 		InitializeComponent();
 	}
-
-    string figure = " ";
+    string MoveFigure = " ";
+    string Coordinates = " ";
+    static string whoseturn = "b";
+    List<string> possiblemoves = new List<string> { };
 
 	public void FieldClicked(object sender, EventArgs e)
 	{
-		Button btnonboard = sender as Button;
-        
-        if (btnonboard.ImageSource != null)
+        Button btnclicked = sender as Button;
+        possiblemoves.Clear();
+        if (MoveFigure.Equals(" "))
         {
-           if (figure != " ")
-           {
-                btn
-           } 
-           else
-           {
-                figure = GetOnlyPath(btnonboard.ImageSource.ToString());
-                btnonboard.ImageSource = "";
-           }
-        } else 
-        {
-            btnonboard.ImageSource = figure;
-            figure = " ";
+            if (btnclicked.ImageSource != null)
+            {
+                if (CheckTurn(GetOnlyPath(btnclicked.ImageSource.ToString())))
+                {              
+                    MoveFigure = GetOnlyPath(btnclicked.ImageSource.ToString());
+                    Coordinates = btnclicked.Text;
+                    btnclicked.ImageSource = " ";
+                }
+            }
         }
-	
+	    else 
+        {
+            FigureAllPossibleMoves(MoveFigure);
+            if (possiblemoves.Contains(btnclicked.Text))
+            {
+                btnclicked.ImageSource = MoveFigure;
+                MoveFigure = " ";
+
+                if (btnclicked.Text != Coordinates)
+                {
+                    UpdateTurn();
+                    Coordinates = " ";
+                }
+            }
+        }
 	}
+
+    private void FigureAllPossibleMoves(string piece)
+    { 
+        string[] specificlocation = Coordinates.Split(' ');
+
+        if (piece.Equals("pawnb.png"))
+        {
+            if (specificlocation[1].Equals("7")) { possiblemoves.Add(specificlocation[0] + " " + 5); }
+            possiblemoves.Add(specificlocation[0] + " " + BlackPawnMath(specificlocation[1]));
+            possiblemoves.Add(specificlocation[0] + " " + specificlocation[1]);
+        }
+        else if (piece.Equals("pawnw.png"))
+        {
+            if (specificlocation[1].Equals("2")) { possiblemoves.Add(specificlocation[0] + " " + 4); }
+            possiblemoves.Add(specificlocation[0] + " " + WhitePawnMath(specificlocation[1]));
+            possiblemoves.Add(specificlocation[0] + " " + specificlocation[1]);
+        }
+        else if (piece.Equals("kingw.png") || piece.Equals("kingb"))
+        {
+            possiblemoves.Add(specificlocation[0] + " " + WhitePawnMath(specificlocation[1]));
+            possiblemoves.Add(specificlocation[0] + " " + BlackPawnMath(specificlocation[1]));
+            possiblemoves.Add(BlackPawnMath(specificlocation[0]) + " " + specificlocation[1]);
+            possiblemoves.Add(WhitePawnMath(specificlocation[0]) + " " + specificlocation[1]);
+            possiblemoves.Add(BlackPawnMath(specificlocation[0]) + " " + WhitePawnMath(specificlocation[1]));
+            possiblemoves.Add(BlackPawnMath(specificlocation[0]) + " " + BlackPawnMath(specificlocation[1]));
+            possiblemoves.Add(WhitePawnMath(specificlocation[0]) + " " + WhitePawnMath(specificlocation[1]));
+            possiblemoves.Add(WhitePawnMath(specificlocation[0]) + " " + BlackPawnMath(specificlocation[1]));
+            possiblemoves.Add(specificlocation[0] + " " + specificlocation[1]);
+        }
+        else if (piece.Equals("rookw.png") || piece.Equals("rookb.png"))
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                possiblemoves.Add(specificlocation[0] + " " + i);
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                possiblemoves.Add(i + " " + specificlocation[1]);
+            }
+        }
+        else if (piece.Equals("bishopw.png") || piece.Equals("bishopb.png")) {
+
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) - i;
+                int num2 = int.Parse(specificlocation[0]) - i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) + i;
+                int num2 = int.Parse(specificlocation[0]) + i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) - i;
+                int num2 = int.Parse(specificlocation[0]) + i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) + i;
+                int num2 = int.Parse(specificlocation[0]) - i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+        }
+        else if (piece.Equals("knightw.png") || piece.Equals("knightb.png"))
+        {
+            possiblemoves.Add(WhitePawnMath(specificlocation[0]) + " " + KnightPlusMath(specificlocation[1]));
+            possiblemoves.Add(WhitePawnMath(specificlocation[0]) + " " + KnightMinusMath(specificlocation[1]));
+            possiblemoves.Add(BlackPawnMath(specificlocation[0]) + " " + KnightPlusMath(specificlocation[1]));
+            possiblemoves.Add(BlackPawnMath(specificlocation[0]) + " " + KnightMinusMath(specificlocation[1]));
+            possiblemoves.Add(KnightMinusMath(specificlocation[0]) + " " + BlackPawnMath(specificlocation[1]));
+            possiblemoves.Add(KnightPlusMath(specificlocation[0]) + " " + BlackPawnMath(specificlocation[1]));
+            possiblemoves.Add(KnightPlusMath(specificlocation[0]) + " " + WhitePawnMath(specificlocation[1]));
+            possiblemoves.Add(KnightMinusMath(specificlocation[0]) + " " + WhitePawnMath(specificlocation[1]));
+        }
+        else if (piece.Equals("queenw.png") || piece.Equals("queenb.png"))
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) - i;
+                int num2 = int.Parse(specificlocation[0]) - i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) + i;
+                int num2 = int.Parse(specificlocation[0]) + i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) - i;
+                int num2 = int.Parse(specificlocation[0]) + i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                int num = int.Parse(specificlocation[1]) + i;
+                int num2 = int.Parse(specificlocation[0]) - i;
+                possiblemoves.Add(num2 + " " + num.ToString());
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                possiblemoves.Add(specificlocation[0] + " " + i);
+            }
+
+            for (int i = 1; i < 9; i++)
+            {
+                possiblemoves.Add(i + " " + specificlocation[1]);
+            }
+        }
+
+        foreach(string move in possiblemoves.ToList())
+        {
+            string[] checker = move.Split(" ");
+            if (int.Parse(checker[0]) > 8 || int.Parse(checker[1]) > 8 || int.Parse(checker[0]) < 1 || int.Parse(checker[1]) < 1)
+            {
+                possiblemoves.Remove(move);
+            }
+        }
+    }
+
+    Func<string, int> BlackPawnMath = pos => int.Parse(pos) - 1;
+
+    Func<string, int> WhitePawnMath = pos => int.Parse(pos) + 1;
+
+    Func<string, int> KnightPlusMath = pos => int.Parse(pos) + 2;
+
+    Func<string, int> KnightMinusMath = pos => int.Parse(pos) - 2;
+
+    private void UpdateTurn()
+    {
+        if (whoseturn.Equals("b")) { whoseturn = "w"; }
+        else  { whoseturn = "b"; }
+    }
+
+    private bool CheckTurn(string source)
+    {
+        string[] ender = source.Split(".");
+        if (ender[0].EndsWith(whoseturn)) { return true; }
+        else { return false; }
+    }
 
     private string GetOnlyPath(string path)
     {
-        string[] onlypath = path.Split(":");
-        return onlypath[1];
+
+        string[] onlypath = path.Split(":");      
+        return onlypath[1].Trim();
     }
 
 	public void StartGame(object sender, EventArgs e)
 	{
-
-		aaa.Text = FileSystem.AppDataDirectory;
         A1.ImageSource = "rookw.png";
 		B1.ImageSource = "knightw.png";
 		C1.ImageSource = "bishopw.png";
@@ -76,5 +243,7 @@ public partial class Chessboard : ContentPage
         F7.ImageSource = "pawnb.png";
         G7.ImageSource = "pawnb.png";
         H7.ImageSource = "pawnb.png";
+
+        btnstart.IsVisible = false;
     }
 }
